40010c407532fd855e72f920393b1107eebe2654
awenBourdon <awen.bourdon@gmail.com>
2025-07-05 01:26:28 +0200

    correction ci qui casse

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -5,7 +5,8 @@ const productController = new ProductController()
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  return await productController.getById(params.id)
+  const { id } = await params
+  return await productController.getById(id)
 }
