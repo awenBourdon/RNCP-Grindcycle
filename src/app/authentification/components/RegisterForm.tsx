dbf5d37bc7f9d3e6ff43e73e353e9318b9578956
awenBourdon <awen.bourdon@gmail.com>
2025-07-06 18:05:00 +0200

    authentification ok

--- src/app/authentification/components/RegisterForm.tsx
+++ src/app/authentification/components/RegisterForm.tsx
@@ -11,7 +11,7 @@ import Spinner from '@/components/Spinner'
 
 type SignUpInput = z.infer<typeof signUpSchema>
 
-const RegisterForm = () => {
+export const RegisterForm = () => {
   const {
     register,
     handleSubmit,
@@ -121,5 +121,3 @@ const RegisterForm = () => {
     </form>
   )
 }
-
-export default RegisterForm
--- src/app/authentification/inscription/succes/page.tsx
+++ src/app/authentification/inscription/succes/page.tsx
@@ -1,6 +1,6 @@
-import ReturnButton from '@/components/ReturnButton'
+import { ReturnButton } from '@/components/ReturnButton'
 
-const InscriptionSuccesPage = () => {
+export default function InscriptionSuccesPage() {
   return (
     <div className="min-h-screen bg-white">
       <div className="max-w-7xl mx-auto px-6 py-40">
@@ -15,18 +15,16 @@ const InscriptionSuccesPage = () => {
           </h1>
 
           <p className="text-xl text-gray-600 mb-12">
-            Un email de vérification t’a été envoyé. Clique sur le lien dans ce
-            message pour activer ton compte.
+            Un email de vérification t&apos;a été envoyé. Clique sur le lien
+            dans ce message pour activer ton compte.
           </p>
 
           <p className="text-gray-500 text-sm mt-8">
-            Tu n’as pas reçu l’email ? Pense à vérifier tes spams ou réessaie
-            plus tard.
+            Tu n&apos;as pas reçu l&rsquo;email ? Pense à vérifier tes spams ou
+            réessaie plus tard.
           </p>
         </div>
       </div>
     </div>
   )
 }
-
-export default InscriptionSuccesPage
