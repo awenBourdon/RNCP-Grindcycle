2ff301ac1e617e6329d4b5dc0905a038b25894b1
awenBourdon <awen.bourdon@gmail.com>
2025-07-12 23:41:41 +0200

    maj route ts product id

--- src/app/api/product/available/route.ts
+++ src/app/api/product/available/route.ts
@@ -1,7 +1,8 @@
 import { ProductController } from '@/lib/server/controllers/productController'
 
-const productController = new ProductController()
+export const dynamic = 'force-dynamic'
 
 export async function GET() {
+  const productController = new ProductController()
   return await productController.getAvailable()
 }
