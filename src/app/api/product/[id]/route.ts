08909a50bdbe1e52deaeaefa3637e7d8ab3c3e55
awenBourdon <awen.bourdon@gmail.com>
2025-07-12 23:37:34 +0200

    maj route ts product id

--- src/app/api/product/[id]/route.ts
+++ src/app/api/product/[id]/route.ts
@@ -1,12 +1,14 @@
 import { NextRequest } from 'next/server'
 import { ProductController } from '@/lib/server/controllers/productController'
 
-const productController = new ProductController()
+export const dynamic = 'force-dynamic'
 
 export async function GET(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) {
+
+  const productController = new ProductController()
   const { id } = await params
   return await productController.getById(id)
 }
