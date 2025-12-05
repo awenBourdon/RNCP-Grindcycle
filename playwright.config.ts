be161804fcb517eb791135c4f6a3324b79a990fe
awenBourdon <awen.bourdon@gmail.com>
2025-12-05 21:54:30 +0100

    Fix: maj next suite a la faille de securite

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,46 +1,20 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
  
 
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 2,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 5,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   timeout: 120000,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
   projects: [
-  {
-    name: 'Création de tests',
-    testDir: './tests',
-    use: { ...devices['Desktop Chrome'] },
-  },
   {
     name: 'e2e-tests',
     testDir: './src/tests/e2e',
@@ -48,7 +22,6 @@ export default defineConfig({
   },
 ],
 
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:3000',
