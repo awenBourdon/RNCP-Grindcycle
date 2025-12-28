e21731f827a3eb49922c30b641819a4eb7c7db89
awenBourdon <awen.bourdon@gmail.com>
2025-12-28 02:37:42 +0100

    Fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,18 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import nextPlugin from '@next/eslint-plugin-next';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-    ignorePatterns: ['src/generated/**/**'],
-  }),
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['src/generated/**', '.next/**', 'node_modules/**'],
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
 ];
-
-export default eslintConfig;
